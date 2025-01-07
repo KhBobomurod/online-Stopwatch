b8590dd25d66ae31896b9e60ae6315bfd4ca6f56
Khudayberganov Bobomurod <khudayberganovbobomurod@gmail.com>
2025-01-07 12:33:30 +0500

    online-stopwatch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Online Stopwatch</h1>
+        <p>hi</p>
+      </div>
+    );
+  }
+}
+export default App;
